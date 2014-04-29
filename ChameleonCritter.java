46dc54d5143c4247a15907af7f2033e3ce556537
Merkuri <bz2118111@gmail.com>
2014-04-29 13:57:56 -0400

    startgit add *

---
+++ ChameleonCritter.java
@@ -0,0 +1,13 @@
+//Barak Zhou
+//https://github.com/Merkuri/gw45-IVV
+//APCS PD 8
+//HW #34
+//2014-04-29
+
+//Exercises: Part 4
+
+/********************************************************
+ * 1. Modify the processActors method in ChameleonCritter
+ * so that if the list of actors to process is empty, the
+ * color of the ChameleonCritter will darken (like a flower).
+ ********************************************************/
